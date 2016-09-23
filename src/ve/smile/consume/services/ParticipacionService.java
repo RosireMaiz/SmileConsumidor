068de07d7e724212eae35be73f8d10626dd1bbbc
RosireMaiz <rosire08@gmail.com>
2016-09-23 09:29:04 -0430

    consumidor de servicio de participa y cartelera

--- src/ve/smile/consume/services/ParticipacionService.java
+++ src/ve/smile/consume/services/ParticipacionService.java
@@ -9,4 +9,11 @@ import ve.smile.payload.response.PayloadParticipacionResponse;
 @Consume("ParticipacionService")
 public class ParticipacionService extends IService<PayloadParticipacionResponse, PayloadParticipacionRequest, Participacion> {
 
+	public PayloadParticipacionResponse consultaAllParticipacion() {
+		return doGet(getUrlService("consultaAllParticipacion"));
+	}
+	
+	public PayloadParticipacionResponse consultaCantidadParticipacion(Integer cantidad) {
+		return doGet(getUrlService("consultaCantidadParticipacion") + "/" + cantidad );
+	}
 }
