3103d0248ed2a4e47730ae928af509a8e9d12834
willandherg <willandherg>
2016-10-11 06:58:37 -0400

    Reporte Voluntario

--- src/ve/smile/consume/services/VoluntarioService.java
+++ src/ve/smile/consume/services/VoluntarioService.java
@@ -4,9 +4,16 @@ import lights.core.annotations.Consume;
 import lights.core.consume.services.IService;
 import ve.smile.dto.Voluntario;
 import ve.smile.payload.request.PayloadVoluntarioRequest;
+import ve.smile.payload.response.PayloadActividadResponse;
 import ve.smile.payload.response.PayloadVoluntarioResponse;
 
 @Consume("VoluntarioService")
 public class VoluntarioService extends IService<PayloadVoluntarioResponse, PayloadVoluntarioRequest, Voluntario> {
+	public PayloadVoluntarioResponse consultaVoluntariosParametrizado(
+			String sql) {
+		return doGet(getUrlService(new StringBuilder()
+				.append("consultaVoluntariosParametrizado")
+				.append("/").append(sql).toString()));
+	};
 
 }
