bb3392052905f58fc5c0219a36a056119c1d2b99
dlcv <dlcv@live.com.ve>
2016-10-16 01:32:42 -0400

    Servicio de la gestión de voluntario: voluntarios por capacitación planificada

--- src/ve/smile/consume/services/VoluntarioService.java
+++ src/ve/smile/consume/services/VoluntarioService.java
@@ -5,15 +5,21 @@ import lights.core.consume.services.IService;
 import ve.smile.dto.Voluntario;
 import ve.smile.payload.request.PayloadVoluntarioRequest;
 import ve.smile.payload.response.PayloadActividadResponse;
+import ve.smile.payload.response.PayloadFortalezaResponse;
 import ve.smile.payload.response.PayloadVoluntarioResponse;
 
 @Consume("VoluntarioService")
-public class VoluntarioService extends IService<PayloadVoluntarioResponse, PayloadVoluntarioRequest, Voluntario> {
-	public PayloadVoluntarioResponse consultaVoluntariosParametrizado(
-			String sql) {
-		return doGet(getUrlService(new StringBuilder()
-				.append("consultaVoluntariosParametrizado")
-				.append("/").append(sql).toString()));
+public class VoluntarioService extends IService<PayloadVoluntarioResponse, PayloadVoluntarioRequest, Voluntario>
+{
+	public PayloadVoluntarioResponse consultaVoluntariosParametrizado(String sql)
+	{
+		return doGet(getUrlService(new StringBuilder().append("consultaVoluntariosParametrizado").append("/").append(sql).toString()));
+	};
+	
+	// CAPACITACION PLANIFICADA
+	public PayloadVoluntarioResponse consultarPorCapacitacionPlanificada(Integer idCapacitacionPlanificada)
+	{
+		return doGet(getUrlService(new StringBuilder().append("consultarPorCapacitacionPlanificada").append("/").append(getIdSesion()).append("/").append(getAccessToken()).append("/").append(idCapacitacionPlanificada).toString()));
 	};
 
 }
