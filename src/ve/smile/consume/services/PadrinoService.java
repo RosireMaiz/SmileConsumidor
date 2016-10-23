89bcbcbbb4f4cd390c186bab972dbee05bedd652
RosireMaiz <rosire08@gmail.com>
2016-10-23 03:47:30 -0400

    Servicio para reporte de padrinos

--- src/ve/smile/consume/services/PadrinoService.java
+++ src/ve/smile/consume/services/PadrinoService.java
@@ -7,9 +7,17 @@ import ve.smile.payload.request.PayloadPadrinoRequest;
 import ve.smile.payload.response.PayloadPadrinoResponse;
 
 @Consume("PadrinoService")
-public class PadrinoService extends IService<PayloadPadrinoResponse, PayloadPadrinoRequest, Padrino> {
+public class PadrinoService extends
+		IService<PayloadPadrinoResponse, PayloadPadrinoRequest, Padrino> {
 
 	public PayloadPadrinoResponse incluirPostuladoPadrino(Padrino padrino) {
 		return doPost(getUrlService("/incluirPostulacionPadrino"), padrino);
 	}
+
+	public PayloadPadrinoResponse consultaPadrinoParametrizado(String sql) {
+		return doGet(getUrlService(new StringBuilder()
+				.append("consultaPadrinoParametrizado").append("/").append(sql)
+				.toString()));
+	}
+
 }
