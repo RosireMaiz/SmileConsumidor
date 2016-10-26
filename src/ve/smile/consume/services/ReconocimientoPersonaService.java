6bfb7fbd1cc4033f52610a06fdae8ad8837847cb
RosireMaiz <rosire08@gmail.com>
2016-10-26 00:27:01 -0400

    Servicio de reconocimientos

--- src/ve/smile/consume/services/ReconocimientoPersonaService.java
+++ src/ve/smile/consume/services/ReconocimientoPersonaService.java
@@ -7,6 +7,14 @@ import ve.smile.payload.request.PayloadReconocimientoPersonaRequest;
 import ve.smile.payload.response.PayloadReconocimientoPersonaResponse;
 
 @Consume("ReconocimientoPersonaService")
-public class ReconocimientoPersonaService extends IService<PayloadReconocimientoPersonaResponse, PayloadReconocimientoPersonaRequest, ReconocimientoPersona> {
+public class ReconocimientoPersonaService
+		extends
+		IService<PayloadReconocimientoPersonaResponse, PayloadReconocimientoPersonaRequest, ReconocimientoPersona> {
+	public PayloadReconocimientoPersonaResponse consultaReconocimientoPersonasParametrizado(
+			String sql) {
+		return doGet(getUrlService(new StringBuilder()
+				.append("consultaReconocimientoPersonasParametrizado")
+				.append("/").append(sql).toString()));
+	};
 
 }
